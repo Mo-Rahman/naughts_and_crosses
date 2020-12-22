c8e63566f9bbffbd0e88f26a78aa1d461dfd562f
Mohammed Rahman <mohammedrahman@Mohammeds-MacBook-Pro.local>
2020-12-22 17:17:58 +0000

    initial skeleton

---
+++ script.js
@@ -0,0 +1,27 @@
+//this is my js file
+
+// function click() {
+//     document.querySelector('.test').addEventListener('click', function() {
+//         let str = 'test';
+//         alert(str);
+//     }
+// }
+
+// document.querySelector('.test').addEventListener('click', function() {
+//   let str = 'test'
+//   alert(str);
+// });
+
+// // regular function
+// function add(a,b) {
+//   return a + b;
+// }
+
+// arrow funciton 
+add = (a,b) => a + b;
+
+// function sub(a,b) {
+//   return a - b;
+// }
+
+
